18efec2f2519b3c3c715d6063028eb5a0a0cba48
Thibaut LENERTZ <t.lenertz@student.helmo.be>
2022-03-31 09:03:59 +0200

    modification path

--- index.php
+++ index.php
@@ -10,9 +10,35 @@
     <link href="css/css.css" rel="stylesheet">
     <title>Deloitte</title>
     <link rel="icon" type="image/x-icon" href="/img/favicon.jpg">
+
 </head>
+<header>
+    <a class="logo" href="index.php"><img src="img/Deloitte-logo-750x375.png" alt="Accueil"></a>
+
+    <nav class="navbar navbar-expand navbar-dark bg-dark" aria-label="nav bar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Change me</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbars02">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Change me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Change link</a>
+                    </li>
+                </ul>
+                <form>
+                    <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+                </form>
+            </div>
+        </div>
+    </nav>
+</header>
 <body>
-<?php include('inc/header.inc.php'); ?>
 
 
 <h1>Ceci est un titre</h1>
@@ -38,6 +64,6 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 -->
-<?php include('inc/footer.inc.php'); ?>
+
 </body>
 </html>
