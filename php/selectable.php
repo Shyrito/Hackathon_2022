b6f766f3949b1e83e168a0f19581a9f7b5030712
e190501 <r.masson3@student.helmo.be>
2022-03-31 16:26:05 +0200

    correction du calendar

--- php/selectable.php
+++ php/selectable.php
@@ -12,9 +12,9 @@ require '../inc/db_link_other.inc.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="../css/css.css" rel="stylesheet">
     <title>Deloitte</title>
-    <link rel="icon" type="image/x-icon" href="/img/favicon.jpg">
+    <link rel="icon" type="image/x-icon" href="../img/favicon.png">
     <link href='../css/css.css' rel='stylesheet' />
-    <script src='../../Informatique/pluri/Hackathon_2022/lib/main.min.js'></script>
+    <script src='../lib/main.min.js'></script>
     <script>
 
   document.addEventListener('DOMContentLoaded', function() {
@@ -26,7 +26,7 @@ require '../inc/db_link_other.inc.php';
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
-      initialDate: '2020-09-12',
+      initialDate: '2022-03-31',
       navLinks: true, // can click day/week names to navigate views
       selectable: true,
       selectMirror: true,
@@ -57,70 +57,29 @@ require '../inc/db_link_other.inc.php';
         $q = $db->prepare("SELECT * FROM MEETING_SLOT m JOIN CANDIDATE c on c.id_candidate = m.fk_candidate JOIN USERS u on u.id_users = m.fk_users JOIN JOB_OFFER j on j.id_job_offer = m.fk_job_offer");
         $q->execute();
         $result1  = $q->rowCount();
-        if($result1 >= 1) {
-        while($result = $q->fetch()) {
-        }
+        if($result1 >= 1) {?>
+        events: [
+       <?php while($result = $q->fetch()) {
 
-        }
-
+            if($result1>=1){
         ?>
 
-      events: [
-        {
-          title: 'All Day Event',
-          start: '2020-09-01'
-        },
-        {
-          title: 'Long Event',
-          start: '2020-09-07',
-          end: '2020-09-10'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2022-04-01T16:00:00'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2020-09-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2020-09-11',
-          end: '2020-09-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2022-04-1T10:30:00',
-          end: '2022-04-1T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2020-09-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2020-09-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: '2020-09-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: '2020-09-12T20:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2020-09-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2020-09-28'
+            {
+                title: '<?php echo $result['title'] ?>',
+                start: '<?php echo $result['start_date'] ?>',
+                end: '<?php echo $result['end_date'] ?>'
+            } <?php if($result1>1){?>,<?php }?>
+
+
+
+
+      <?php
+            }
+       }?>
+        ]
+<?php
         }
-      ]
+        ?>
     });
 
     calendar.render();
