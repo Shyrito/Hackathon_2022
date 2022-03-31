0a2540d79c2d299ea47eb26af9af08cb97ed6e99
Thibaut LENERTZ <t.lenertz@student.helmo.be>
2022-03-31 10:10:14 +0200

    insert Candidate into bd

--- php/index.php
+++ php/index.php
@@ -7,36 +7,15 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="css/css.css" rel="stylesheet">
+    <link href="../css/css.css" rel="stylesheet">
     <title>Deloitte</title>
     <link rel="icon" type="image/x-icon" href="/img/favicon.jpg">
 
 </head>
 <header>
-    <a class="logo" href="index.php"><img src="img/Deloitte-logo-750x375.png" alt="Accueil"></a>
-
-    <nav class="navbar navbar-expand navbar-dark bg-dark" aria-label="nav bar">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Change me</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbars02">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Change me</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Change link</a>
-                    </li>
-                </ul>
-                <form>
-                    <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-                </form>
-            </div>
-        </div>
-    </nav>
+    <?php
+    include('inc/header.inc.php');
+    ?>
 </header>
 <body>
 <?php
